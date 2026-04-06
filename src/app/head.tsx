ae3f6cf4ec7a85d6d6ece43db630f09c399d9829
Akil Fernando <akilfernando2003@gmail.com>
2026-04-06 11:59:09 +0530

    still trying to fix the favicon issue, added multiple icon formats and sizes to see if that resolves the problem.

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,8 +1,10 @@
 export default function Head() {
   return (
     <>
-      <link rel="icon" href="/logo.png" />
-      <link rel="shortcut icon" href="/logo.png" />
+      <link rel="icon" href="/logo.png" type="image/png" sizes="any" />
+      <link rel="shortcut icon" href="/logo.png" type="image/png" />
+      <link rel="apple-touch-icon" href="/logo.png" />
+      <link rel="mask-icon" href="/logo.png" color="#4f46e5" />
     </>
   );
 }
